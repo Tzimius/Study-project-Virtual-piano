b70dfcf1de953c6de158041676b3decd4750dc47
Tzimius <tzzimi@gmail.com>
2020-04-06 18:17:01 +0300

    Add files via upload

--- beh.js
+++ beh.js
@@ -1,11 +1,10 @@
-const audioKeys = ["A.mp3","S.mp3","D.mp3","F.mp3","G.mp3","H.mp3","J.mp3","W.mp3","E.mp3","T.mp3","Y.mp3","U.mp3"];
 let pianoKeys = "asdfghjwetyu";
 document.addEventListener("keypress",function (event) {
     let varKey = event.key;
     for (let i = 0;i<pianoKeys.length;i++){
         if (varKey == pianoKeys[i]) {
             console.log(varKey);
-            createSound("sound/"+audioKeys[i]);
+            createSound("sound/"+pianoKeys[i].toUpperCase()+".mp3");
         }
     }
     if (!pianoKeys.match(varKey)){
